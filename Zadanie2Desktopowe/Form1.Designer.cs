42c21260638c96ccf887c732b99121c2e601faa5
Visual Student <visualstudent@zschie.edu.pl>
2022-10-17 09:24:36 +0200

    tak.

--- Zadanie2Desktopowe/Form1.Designer.cs
+++ Zadanie2Desktopowe/Form1.Designer.cs
@@ -28,20 +28,150 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(93, 70);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Użytkownik";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(136, 121);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hasło";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(239, 121);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(200, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(239, 70);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(200, 20);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(94, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(101, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Weryfikacja";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(137, 241);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(324, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "wpisz znaki, które widzisz na poniższym obrazku, aby kontynuować";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(98, 298);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(120, 31);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(239, 309);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(182, 20);
+            this.textBox3.TabIndex = 7;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Black;
+            this.linkLabel1.Location = new System.Drawing.Point(236, 343);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(56, 13);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "losuj nowy";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(467, 379);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Zaloguj";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(633, 471);
+            this.ClientSize = new System.Drawing.Size(612, 441);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Sekretariat";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Zadanie2Desktopowe/Form1.cs
+++ Zadanie2Desktopowe/Form1.cs
@@ -12,9 +12,105 @@ namespace Zadanie2Desktopowe
 {
     public partial class Form1 : Form
     {
+        public Image obr1 = Image.FromFile(@"C:\Users\student\source\repos\Zadanie2Desktopowe\1.png");
+        public Image obr2 = Image.FromFile(@"C:\Users\student\source\repos\Zadanie2Desktopowe\2.png");
+        public Image obr3 = Image.FromFile(@"C:\Users\student\source\repos\Zadanie2Desktopowe\3.png");
+        public Image obr4 = Image.FromFile(@"C:\Users\student\source\repos\Zadanie2Desktopowe\4.png");
+        public Image obr5 = Image.FromFile(@"C:\Users\student\source\repos\Zadanie2Desktopowe\5.png");
+        public Image obr6 = Image.FromFile(@"C:\Users\student\source\repos\Zadanie2Desktopowe\6.png");
         public Form1()
         {
             InitializeComponent();
+        }
+        private void losowanie_obrazka()
+        {
+            Random generator = new Random();
+            int a = generator.Next(1, 6);
+            switch (a)
+            {
+                case 1:
+                    pictureBox1.Image = obr1;
+                    break;
+                case 2:
+                    pictureBox1.Image = obr2;
+                    break;
+                case 3:
+                    pictureBox1.Image = obr3;
+                    break;
+                case 4:
+                    pictureBox1.Image = obr4;
+                    break;
+                case 5:
+                    pictureBox1.Image = obr5;
+                    break;
+                case 6:
+                    pictureBox1.Image = obr6;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var popr_nazwa = "admin";
+            var popr_haslo = "Qwerty1@34";
+            var nazwa = textBox1.Text;
+            var haslo = textBox2.Text;
+            var textobr = "";
+            if(pictureBox1.Image == obr1)
+            {
+                textobr = "mxyxw";
+            }
+            if (pictureBox1.Image == obr2)
+            {
+                textobr = "b5nmm";
+            }
+            if (pictureBox1.Image == obr3)
+            {
+                textobr = "74853";
+            }
+            if (pictureBox1.Image == obr4)
+            {
+                textobr = "cg5dd";
+            }
+            if (pictureBox1.Image == obr5)
+            {
+                textobr = "x3deb";
+            }
+            if (pictureBox1.Image == obr6)
+            {
+                textobr = "befhd";
+            }
+
+            if(nazwa == popr_nazwa)
+            {
+                if(haslo == popr_haslo)
+                {
+
+                }
+                else
+                {
+                    MessageBox.Show("Nie poprawne hasloo");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Nie poprawna nazwa");
+            }
+
+
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            losowanie_obrazka();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            losowanie_obrazka();
         }
     }
 }
