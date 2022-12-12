3b217152b43950de2d427eb1ba6b5fd3c75f099e
Visual Student <visualstudent@zschie.edu.pl>
2022-12-12 07:24:15 +0100

    odczyt zrobiony

--- Zadanie2Desktopowe/Form2.cs
+++ Zadanie2Desktopowe/Form2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,12 +21,63 @@ namespace Zadanie2Desktopowe
 
         private void button1_Click(object sender, EventArgs e)
         {
+           
             StreamReader sr = new StreamReader("C:\\Users\\student\\source\\repos\\Kakaoss\\Zadanie2Desktopowe\\uczen.txt");
-
+            string[][] tablica;
+            string linia;
+            string wynik = "";
+            List<string[]> lista = new List<string[]>();
+            while ((linia = sr.ReadLine()) != null)
+            {
+                string[] tablica2 = linia.Split(';');
+                lista.Add(tablica2);
+            }
+            tablica = lista.ToArray();
+            sr.Close();
+            int i;
             if (comboBox1.Text == "Imie")
             {
-                
+                i = 1;
             }
+            else if (comboBox1.Text == "Nazwisko")
+            {
+                i = 2;
+            }
+            else
+            {
+                i = 3;
+            }
+            if (comboBox2.Text == "jest równe")
+            {
+                for (int j = 1; j < tablica.Length; j++)
+                {
+                    if (tablica[j][i] == textBox4.Text)
+                    {
+                        wynik += tablica[j][0] + ";" + tablica[j][1] + ";" + tablica[j][2] + ";" + tablica[j][3] + Environment.NewLine;
+                    }
+                }
+            }
+            else if (comboBox2.Text == "rozpoczyna sie od")
+            {
+                for (int j = 1; j < tablica.Length; j++)
+                {
+                    if (tablica[j][i].StartsWith(textBox4.Text))
+                    {
+                        wynik += tablica[j][0] + ";" + tablica[j][1] + ";" + tablica[j][2] + ";" + tablica[j][3] + Environment.NewLine;
+                    }
+                }
+            }
+            else
+            {
+                for (int j = 1; j < tablica.Length; j++)
+                {
+                    if (tablica[j][i].Contains(textBox4.Text))
+                    {
+                        wynik += tablica[j][0] + ";" + tablica[j][1] + ";" + tablica[j][2] + ";" + tablica[j][3] + Environment.NewLine;
+                    }
+                }
+            }
+            richTextBox1.Text = wynik;
         }
 
         private void button2_Click(object sender, EventArgs e)
